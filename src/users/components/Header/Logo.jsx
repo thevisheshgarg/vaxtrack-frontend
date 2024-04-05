13ead1156ae541a4b14d1d701f645db2b232612e
vishesh garg <visheshgarg5872@gmail.com>
2024-04-05 20:11:13 +0530

    Added logo

--- src/users/components/Header/Logo.jsx
+++ src/users/components/Header/Logo.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <a href="/" className="text-white font-bold text-xl">
-      VaxTrack
+    <a href="/" className="flex items-center text-white font-bold text-xl">
+      <img src="Logo.png" alt="Logo" className="max-w-12 max-h-12 mr-2" />
+      <span>VaxTrack</span>
     </a>
   );
 };
